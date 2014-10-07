1fda1c1863aa4e532463daec55aae08316d1f680
Endika Gutiérrez <me@endika.net>
2014-10-07 14:36:07 +0200

    Initial Commit

---
+++ Wiboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wiboard
+//
+//  Created by Endika Gutiérrez Salas on 07/10/14.
+//  Copyright (c) 2014 Endika Gutiérrez Salas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
