a8fbbc0d381c5b3e141099e4a41684792850e8ef
Endika Gutiérrez <me@endika.net>
2014-10-07 20:42:18 +0200

    Basic keyboard works

---
+++ Wiboard-Keyboard/KeyboardView.swift
@@ -0,0 +1,14 @@
+//
+//  KeyboardView.swift
+//  Wiboard
+//
+//  Created by Endika Gutiérrez Salas on 07/10/14.
+//  Copyright (c) 2014 Endika Gutiérrez Salas. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardView: UIView {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+}
--- Wiboard-Keyboard/KeyboardViewController.swift
+++ Wiboard-Keyboard/KeyboardViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class KeyboardViewController: UIInputViewController {
 
-    @IBOutlet var nextKeyboardButton: UIButton!
+    var keyboardView: KeyboardView!
 
     override func updateViewConstraints() {
         super.updateViewConstraints()
@@ -20,21 +20,15 @@ class KeyboardViewController: UIInputViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton.buttonWithType(.System) as UIButton
-    
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.setTranslatesAutoresizingMaskIntoConstraints(false)
-    
-        self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-    
-        var nextKeyboardButtonLeftSideConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
-        var nextKeyboardButtonBottomConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
-        self.view.addConstraints([nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint])
+
+        var nib = UINib(nibName: "KeyboardView", bundle: nil)
+        let objects = nib.instantiateWithOwner(self, options: nil)
+        keyboardView = objects[0] as KeyboardView
+        keyboardView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        self.view.addSubview(keyboardView)
+
+        keyboardView.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,15 +42,7 @@ class KeyboardViewController: UIInputViewController {
 
     override func textDidChange(textInput: UITextInput) {
         // The app has just changed the document's contents, the document context has been updated.
-    
-        var textColor: UIColor
-        var proxy = self.textDocumentProxy as UITextDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
-            textColor = UIColor.whiteColor()
-        } else {
-            textColor = UIColor.blackColor()
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+
     }
 
 }
